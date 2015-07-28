6f025d7d0220b50d072b841883ebe17a86101520
Sean Enderby <sean.enderby@gmail.com>
2015-07-28 15:53:57 +0100

    docs for librdfholder

--- SAFE_juce_module/PluginUtils/LibrdfHolder.h
+++ SAFE_juce_module/PluginUtils/LibrdfHolder.h
@@ -1,15 +1,24 @@
 #ifndef LIB_RDF_HOLDER_H_INCLUDED
 #define LIB_RDF_HOLDER_H_INCLUDED
 
+/** 
+ *  A class for holding onto some useful librdf things.
+ */
 struct LibrdfHolder
 {
+    //==========================================================================
+    //      Constructor and Destructor
+    //==========================================================================
+    /** Create a new feature extractor. */
     LibrdfHolder();
+
+    /** Destructor */
     ~LibrdfHolder();
 
-    librdf_world *world;
-    librdf_storage *storage;
-    librdf_model *model;
-    librdf_serializer *serializer;
+    librdf_world *world; /**< An instance of a librdf world object. */
+    librdf_storage *storage; /**< An instance of an in memory librdf storage object. */
+    librdf_model *model; /**< An instance of a librdf model object. */
+    librdf_serializer *serializer; /**< A librdf serializer for the turtle syntax. */
 };
 
 #endif // LIB_RDF_HOLDER_H_INCLUDED
